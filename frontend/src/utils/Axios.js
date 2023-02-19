4b966327f62bb21caef858fa5f5eff81919bcc55
poornesh <poorneshchenna29@gmail.com>
2023-02-19 21:57:01 +0530

    backend url changed

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
+  baseURL: 'https://medsurplus-backend.onrender.com',
 })
 
 instance.interceptors.request.use((req) => {
